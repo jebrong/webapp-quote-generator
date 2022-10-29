8c4fc69b8b3d1f6c281752fe01642f62e519f9c2
jebrong <92987206+jebrong@users.noreply.github.com>
2022-10-29 15:51:50 +0800

    finished js

---
+++ script.js
@@ -0,0 +1,47 @@
+const author = document.querySelector("#author");
+const quote = document.querySelector("#quote");
+const quoteButton = document.querySelector("#new-quote");
+const twitterButton = document.querySelector("#twitter");
+
+let apiQuotes = [];
+
+const addQuote = () => {
+  let random = Math.floor(Math.random() * apiQuotes.length);
+
+  let selectedQuote = apiQuotes[random].text;
+  let selectedAuthor = "";
+
+  apiQuotes[random].author === null
+    ? (selectedAuthor = "Anonymous")
+    : (selectedAuthor = apiQuotes[random].author);
+
+  selectedQuote.length > 50
+    ? quote.classList.add("long-quote")
+    : quote.classList.remove("long-quote");
+
+  quote.innerText = selectedQuote;
+  author.innerText = selectedAuthor;
+};
+
+const getQuotes = async () => {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    let response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+
+    addQuote();
+  } catch (error) {}
+};
+
+const tweetQuote = () => {
+  window.open(
+    `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`,
+    "_blank"
+  );
+};
+
+getQuotes();
+
+quoteButton.addEventListener("click", addQuote);
+
+twitterButton.addEventListener("click", tweetQuote);
